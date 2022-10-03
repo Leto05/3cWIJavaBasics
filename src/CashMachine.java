db4273b21d44c4126f9660b06f66347af5620e24
Leto05 <leto.harm@student.htldornbirn.at>
2022-10-03 12:56:22 +0200

    refactoring

---
+++ src/CashMachine.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class CashMachine {
+    public static void main(String[] args) {
+        int Balance= 0;
+
+        Scanner scanner = new Scanner(System.in);
+        int userSelection =0;
+        do {
+
+            System.out.println("Was moechten Sie tun?");
+            System.out.println("1 Geld einzahlen");
+            System.out.println("2 Geld abheben");
+            System.out.println("3 Kontostand einsehen");
+            System.out.println("4 Vorgang beenden");
+
+            userSelection = scanner.nextInt();
+
+            if (userSelection == 1) {
+                System.out.println("Wie viel moechten Sie einzahlen?");
+                int moneyAmount = scanner.nextInt();
+                Balance = Balance + moneyAmount;
+                System.out.println("Sie haben " + moneyAmount + " Euro eingezahlt");
+            }
+            if (userSelection == 2) {
+                System.out.println("Wie viel moechten Sie abheben?");
+                int moneyAmount = scanner.nextInt();
+                Balance = Balance - moneyAmount;
+                System.out.println("Sie haben " + moneyAmount + " Euro ausgezahlt");
+            }
+            if (userSelection == 3) {
+                System.out.println("Ihr Kontostand beträgt:");
+                System.out.println(Balance);
+            }
+        }while (userSelection != 4);
+    }
+}
---
+++ src/DiceGame.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class DiceGame {
+    public static void main(String[] args) {
+        //Würfelspiel
+        System.out.println("Wuerfelspiel");
+        System.out.println("Jeder Spieler hat 6 Wuerfel. Bei diesem Spiel wird gegen den Computer gewürfelt. Wer eine höhere Anzahl gewürfelt hat, gewinnt!");
+        System.out.println("Fuer Spielstart drücke: 1 .");
+        Random random = new Random();
+        Scanner scanner=new Scanner(System.in);
+        int userselection= 0;
+        int playerresult = 0;
+        int botresult = 0;
+
+
+
+        userselection= scanner.nextInt();
+        if(userselection==1)
+        {
+
+
+        }
+        else
+        {
+
+        }
+
+    }
+}
