813bf077c708c3ea9a851b041340b3eb39986c49
Leto05 <leto.harm@student.htldornbirn.at>
2022-10-06 09:28:49 +0200

    If Statement gemacht, Dice Game weiterprogrammiert(nicht fertig)

--- src/DiceGame.java
+++ src/DiceGame.java
@@ -2,24 +2,50 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class DiceGame {
-    public static void main(String[] args) {
+
+        public static void main(String[] args) {
         //Würfelspiel
         System.out.println("Wuerfelspiel");
         System.out.println("Jeder Spieler hat 6 Wuerfel. Bei diesem Spiel wird gegen den Computer gewürfelt. Wer eine höhere Anzahl gewürfelt hat, gewinnt!");
-        System.out.println("Fuer Spielstart drücke: 1 .");
+        System.out.println("Fuer Spielstart drücke: 1");
         Random random = new Random();
         Scanner scanner=new Scanner(System.in);
-        int userselection= 0;
-        int playerresult = 0;
-        int botresult = 0;
+        int DiceRollsLeft =6;
+        int UserSelection= 0;
+        int PlayerResult = 0;
+        int BotResult = 0;
+        int PlayerRolls[]={0,0,0,0,0,0};
+        int PlayersTurn=0;
 
-
-
-        userselection= scanner.nextInt();
-        if(userselection==1)
+        UserSelection= scanner.nextInt();
+        if(UserSelection==1)
         {
+                System.out.println("Gebe eine 1 ein, um zu wuerfeln:");
+                do{
+                        PlayersTurn= scanner.nextInt();
+                        PlayerRolls[DiceRollsLeft-1]= random.nextInt(1, 7 );
+                        System.out.print("Du hast eine "); System.out.print(PlayerRolls[DiceRollsLeft-1]);
+                        System.out.println(" gewuerfelt");
+                        DiceRollsLeft=DiceRollsLeft-1;
 
-
+                }while(DiceRollsLeft != 0);
+                for(int i=0;i<6;i++)
+                {
+                        PlayerResult=PlayerResult+PlayerRolls[i];
+                }
+                System.out.print("Deine Wuerfelsumme liegt bei: ");
+                System.out.println(PlayerResult);
+                BotResult= random.nextInt(6,37);
+                System.out.print("Die Wuerfelsumme des Computers liegt bei: ");
+                System.out.println(BotResult);
+                if(PlayerResult==BotResult){
+                        System.out.println("Unentschieden");
+                }
+                if(PlayerResult>BotResult){
+                        System.out.println("Du hast gewonnen!");
+                } else if (PlayerResult<BotResult) {
+                        System.out.println("Du hast verloren!");
+                }
         }
         else
         {
---
+++ src/IfStatement.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+public class IfStatement
+{
+    public static void main(String[] args) {
+        Random random = new Random();
+        int number = random.nextInt(100);
+        int number2 = random.nextInt(100);
+
+        System.out.println(number);
+        System.out.println(number2);
+
+
+        if (number < 50 && number != 50)
+        {
+            System.out.println("Erste Zahl klein, zweite kein 50ger");
+        }
+        if (number < 30 || number2 < 30) {
+            System.out.println("Eine der beiden Zahlen ist kleiner als 30");
+        }
+        if (number < number2 && number < 50) {
+            System.out.println("Zahl 1 ist kleiner als Zahl 2 und Mini");
+        }
+    }
+}
